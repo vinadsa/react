061af335a2351baa8c31e5adef274902c4fd5f13
vinadsa <kevinadsantoso@gmail.com>
2025-05-17 19:20:11 +0700

    Header

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -1,24 +1,24 @@
+import Search from './components/search.jsx'
 import { useState } from "react"
 
-const Card = ({title}) => {
-  const [hasLiked, setHasLiked] = useState(false);
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('')
   return(
-    <div className="card">
-      <h2>{title}</h2>
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? 'Liked' : 'Like'}
-      </button>
-    </div>
+    <main>
+      <div className="pattern"/>
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>Find <span className="text-gradient">
+            Movies</span> You'll Enjoy Without The Hassle
+          </h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />  
+      </div>
+    </main>
   )
 }
 
-const App = () => {
-  return (
-    <div className="card-container">
-      <Card title="Star Wars" />
-      <Card title="Lion King" />
-      <Card title="Avengers" />
-    </div>
-  )
-}
-export default App
+export default App;
---
+++ first-react/src/components/search.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+const Search = ({searchTerm, setSearchTerm}) => {
+    return(
+        <div className='search'>
+            <div>
+                <img src="search.svg" alt="search" />
+                <input 
+                type="text" 
+                placeholder='Search Through Thousands of Movies' 
+                value={searchTerm}
+                onChange={(event) => setSearchTerm(event.target.value)}/>                
+            </div>
+        </div>
+    )
+}
+
+export default Search
